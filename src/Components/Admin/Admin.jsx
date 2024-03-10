ff3b3e6344d4cdc995c084618386e7a2409739d0
TanishqGupta <76784859+TanishqGupta12@users.noreply.github.com>
2024-03-10 09:06:55 +0530

    filx it

--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -1,14 +1,11 @@
 import React, { useState } from "react";
-
 import "./Admin.css";
 import { Link } from "react-router-dom";
 import ChecklistIcon from "@mui/icons-material/Checklist";
-import AddIcon from "@mui/icons-material/Add";
 
 import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
-// import { ref as refs, set, push } from "firebase/database";
-import  {storage , Database} from '../../Firebase';
-
+import { ref as refs, set, push } from "firebase/database";
+import { storage, Database } from "../../Firebase";
 
 export default function Admin() {
   const [status, Setstatus] = useState({
@@ -17,113 +14,165 @@ export default function Admin() {
     Details: "",
   });
 
-  const [file, setfile] = useState(null);
   const [imgUrl, setImgUrl] = useState(null);
+  const [image, setimage] = useState("");
 
-   const uploadFile = () => {
-    if (!file) return;
-    const storageRef = ref(storage, `files/${file.name}`);
-    const uploadTask = uploadBytesResumable(storageRef, file);
+  const [file, setfile] = useState(null);
+  const [fileUrl, setfileUrl] = useState("");
 
-    uploadTask.on(
+  const sendSubmit = () => {
+    const dbRef = push(refs(Database, "Book"));
+    console.log(imgUrl);
+    // Push data to the database
+    set(dbRef, {
+      Name: status.Book,
+      Category: status.Category,
+      Details: status.Category,
+      file: {
+        image: imgUrl,
+        file: fileUrl,
+      },
+    });
+  };
+
+  const uploadFile = (e) => {
+    e.preventDefault();
+
+    if (!image && !file) return;
+
+    const fileStorageRef = ref(
+      storage,
+      `files/${status.Category}/${file.name}`
+    );
+    const imageStorageRef = ref(
+      storage,
+      `images/${status.Category}/${image.name}`
+    );
+
+    const fileUploadTask = uploadBytesResumable(fileStorageRef, file);
+    const imageUploadTask = uploadBytesResumable(imageStorageRef, image);
+
+    imageUploadTask.on(
       "state_changed",
       (snapshot) => {
-        const progress =
-          Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
         console.log(progress);
       },
       (error) => {
         alert(error);
       },
       () => {
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        getDownloadURL(imageUploadTask.snapshot.ref).then((downloadURL) => {
           setImgUrl(downloadURL);
+          console.log(downloadURL);
         });
       }
-      );
-      alert("Data successfully added to Firestore")
-      console.log(imgUrl)
-      // sendSubmit();
+    );
+
+    fileUploadTask.on(
+      "state_changed",
+      (snapshot) => {
+        const progress = Math.round(
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+        );
+        console.log(progress);
+      },
+      (error) => {
+        alert(error);
+      },
+      () => {
+        getDownloadURL(fileUploadTask.snapshot.ref).then((downloadURL) => {
+          setfileUrl(downloadURL);
+          console.log(downloadURL);
+        });
+      }
+    );
   };
 
   const handleFileChange = (e) => {
+    setimage(e.target.files[0]);
     setfile(e.target.files[0]);
   };
 
   const handleStatus = (e) => {
     const { name, value } = e.target;
-    // console.log({ name, value });
     Setstatus((prevStatus) => ({
       ...prevStatus,
       [name]: value,
     }));
-
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault(); 
-    uploadFile();
+    e.preventDefault();
+    sendSubmit();
   };
+
   return (
     <>
-    <div className="main_admin">
-      <div className="admin_form">
-        <form onSubmit={handleSubmit}>
-          <div>
-            <h2> Book Name </h2>
-            <input
-              type="text"
-              placeholder="Name"
-              value={status.Book}
-              onChange={handleStatus}
-              name="Book"
-            />
-          </div>
-          <div>
-            <h2> Book Category </h2>
-            <input
-              type="text"
-              placeholder="Category"
-              value={status.Category}
-              onChange={handleStatus}
-              name="Category"
-            />
-          </div>
-          <div>
-            <h2> Book Details </h2>
-            <textarea
-              placeholder="Details"
-              id="w3review"
-              name="Details"
-              rows="4"
-              cols="80"
-              value={status.Details}
-              onChange={handleStatus}
-            ></textarea>
-          </div>
-          <div>
+      <div className="main_admin">
+        <div className="admin_form">
+          <form className="upload" onSubmit={uploadFile}>
             <input
               placeholder="file"
               type="file"
               onChange={handleFileChange}
               name="file"
-              multiple
             />
+
+            <input
+              placeholder="file"
+              type="file"
+              onChange={handleFileChange}
+              name="file"
+            />
+            <input type="submit" value="upload" />
+          </form>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <h2> Book Name </h2>
+              <input
+                type="text"
+                placeholder="Name"
+                value={status.Book}
+                onChange={handleStatus}
+                name="Book"
+              />
+            </div>
+            <div>
+              <h2> Book Category </h2>
+              <input
+                type="text"
+                placeholder="Category"
+                value={status.Category}
+                onChange={handleStatus}
+                name="Category"
+              />
+            </div>
+            <div>
+              <h2> Book Details </h2>
+              <textarea
+                placeholder="Details"
+                id="w3review"
+                name="Details"
+                rows="4"
+                cols="80"
+                value={status.Details}
+                onChange={handleStatus}
+              ></textarea>
+            </div>
+            <div>
+              <input type="submit" value="Submit" />
+            </div>
+          </form>
+          <div className="icon">
+            <Link to="/List">
+              <ChecklistIcon />
+            </Link>
           </div>
-          <div>
-            <input type="submit" value="Submit" />
-          </div>
-        </form>
-        <div className="icon">
-          <Link to="/List">
-            <ChecklistIcon/>
-          </Link>
-          <Link to="/admin">
-            <AddIcon />
-          </Link>
         </div>
       </div>
-    </div>   
-  </>
+    </>
   );
 }
--- src/util/form.jsx
+++ src/util/form.jsx
@@ -1,59 +1,103 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Typography from "@mui/material/Typography";
+
+import { ref as refs, set, push, get } from "firebase/database";
+
+import { Database } from "../Firebase";
 
 export default function Form() {
   const [Full_name, setFull_name] = useState("");
-  const [email, setemail] = useState(" ");
-  const [password, setpassword] = useState(" ");
-  const [c_password, setc_password] = useState(" ");
-  const [P_number, setP_number] = useState();
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [c_password, setc_password] = useState("");
+
+  const [getdata, setdata] = useState();
 
   const handleButtonClick = (e) => {
-    setemail(e.target.value);
-    setFull_name(e.target.value);
-    setpassword(e.target.value);
-    setc_password(e.target.value);
-    setP_number(e.target.value);
+    e.preventDefault();
+
+    const dbRef = push(refs(Database, "sign_up"));
+    // Push data to the databas
+    set(dbRef, {
+      Name: Full_name,
+      email: email,
+      password: password,
+      c_password: c_password,
+      stutas: true,
+    });
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const snapshot = await get(refs(Database, "sign_up"));
+
+      snapshot.val() !== null
+        ? setdata(Object.values(snapshot.val()))
+        : setdata("No data available in the database");
+
+      // console.log(snapshot.exists());
+    };
+
+    fetchData();
+    console.log(getdata);
+  }, [handleButtonClick]);
+
   return (
     <>
       <div id="sign">
         <Typography variant="h6" display="block" className="h6">
           Full Name
         </Typography>
-        <input type="text" onChange={handleButtonClick} value={Full_name} />
+        <input
+          type="text"
+          onChange={(e) => {
+            setFull_name(e.target.value);
+          }}
+          value={Full_name}
+        />
       </div>
 
       <div>
         <Typography variant="h6" display="block" className="h6">
           Email
         </Typography>
-        <input type="email" onChange={handleButtonClick} value={email} />
+        <input
+          type="email"
+          onChange={(e) => {
+            setemail(e.target.value);
+          }}
+          value={email}
+        />
       </div>
 
       <div>
         <Typography variant="h6" display="block" className="h6">
           Password
         </Typography>
-        <input type="password" onChange={handleButtonClick} value={password} />
+        <input
+          type="password"
+          onChange={(e) => {
+            setpassword(e.target.value);
+          }}
+          value={password}
+        />
       </div>
 
       <div id="sign">
         <Typography variant="h6" display="block" className="h6">
           Confirm Password
         </Typography>
-        <input type="password" onChange={handleButtonClick} value={c_password} />
-      </div>
-
-      <div id="sign">
-        <Typography variant="h6" display="block" className="h6">
-          Phone Number
-        </Typography>
-        <input type="number" onChange={handleButtonClick} value={P_number} />
+        <input
+          type="password"
+          onChange={(e) => {
+            setc_password(e.target.value);
+          }}
+          value={c_password}
+        />
       </div>
 
       <div>
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Submit" onClick={handleButtonClick} />
       </div>
     </>
   );
