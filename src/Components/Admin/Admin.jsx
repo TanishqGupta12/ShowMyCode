90a5a9505dc3a6110ac281b34e3918d5a76d1f90
TanishqGupta <76784859+TanishqGupta12@users.noreply.github.com>
2024-03-23 17:06:55 +0530

    remover committed

--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -28,76 +28,6 @@ export default function Admin() {
     alert("data successfully uploaded");
   };
 
-  const uploadFile = async () => {
-    if (!file) {
-      alert("Please select an a file to upload.");
-      return;
-    }
-
-    try {
-      const fileStorageRef = ref(
-        storage,
-        `files/${status.Category}/${file.name}`
-      );
-
-      const fileUploadTask = uploadBytesResumable(fileStorageRef, file);
-
-      fileUploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          // const progress = Math.round(
-          //     (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-          // );
-          // console.log("File Upload Progress: ", progress + "%");
-          var imageUploadTaskData = getDownloadURL(snapshot.ref);
-          setfileUrl(imageUploadTaskData);
-        },
-        (error) => {
-          throw new Error("File upload failed: " + error.message);
-        }
-      );
-      alert(" File successfully uploaded");
-    } catch (error) {
-      console.error("Upload failed:", error.message);
-      alert("Upload failed: " + error.message);
-    }
-  };
-
-  const uploadImage = async () => {
-    if (!image) {
-      alert("Please select an image  to upload.");
-      return;
-    }
-
-    try {
-      const imageStorageRef = ref(
-        storage,
-        `images/${status.Category}/${image.name}`
-      );
-
-      const imageUploadTask = uploadBytesResumable(imageStorageRef, image);
-
-      imageUploadTask.on(
-        "state_changed",
-        (snapshot) => {
-          // const progress = Math.round(
-          //     (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-          // );
-          // console.log("Image Upload Progress: ", progress + "%");
-          var imageUploadTaskData = getDownloadURL(snapshot.ref);
-          setImgUrl(imageUploadTaskData);
-        },
-        (error) => {
-          throw new Error("Image upload failed: " + error.message);
-        }
-      );
-
-      alert("Image  successfully uploaded");
-    } catch (error) {
-      console.error("Upload failed:", error.message);
-      alert("Upload failed: " + error.message);
-    }
-  };
 
   const handleFileChange = (e) => {};
 
@@ -116,67 +46,9 @@ export default function Admin() {
 
   return (
     <>
+        <Aside/>
       <div className="main_admin">
         <div className="admin_form">
-          {/* <form className="upload" onSubmit={uploadFile}> */}
-          <table class="table">
-            <tr>
-              <td scope="row">Book file</td>
-              <td>
-                <input
-                  placeholder="file"
-                  type="file"
-                  onChange={(e) => {
-                    setfile(e.target.files[0]);
-                  }}
-                  name="file"
-                />
-              </td>
-              <td>
-                <input type="submit" value="upload" onClick={uploadFile} />
-              </td>
-            </tr>
-            <tr>
-              <td scope="row">Book image</td>
-              <td>
-                <input
-                  placeholder="image"
-                  type="file"
-                  onChange={(e) => {
-                    setimage(e.target.files[0]);
-                  }}
-                  name="file"
-                />
-              </td>
-              <td>
-                <input type="submit" value="upload" onClick={uploadImage} />
-              </td>
-            </tr>
-          </table>
-          {/* <div>
-              <h4> Book file </h4>
-              <input
-                placeholder="file"
-                type="file"
-                onChange={(e) => {
-                  setfile(e.target.files[0]);
-                }}
-                name="file"
-              />
-            </div>
-            <div>
-              <h4> Book image </h4>
-              <input
-                placeholder="image"
-                type="file"
-                onChange={(e) => {
-                  setimage(e.target.files[0]);
-                }}
-                name="file"
-              />
-            </div> */}
-          {/* <input type="submit" value="upload" /> */}
-          {/* </form> */}
           <form onSubmit={handleSubmit}>
             <div>
               <h2> Book Name </h2>
@@ -214,13 +86,8 @@ export default function Admin() {
               <input type="submit" value="Submit" />
             </div>
           </form>
-          <div className="icon">
-            <Link to="/List">
-              <ChecklistIcon />
-            </Link>
-          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
