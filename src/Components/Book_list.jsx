83ced2601b9c3b3143083a75fac6f0d1f08ff66a
TanishqGupta <76784859+TanishqGupta12@users.noreply.github.com>
2024-05-02 21:02:27 +0530

    fix merge

--- src/Components/Book_list.jsx
+++ src/Components/Book_list.jsx
@@ -51,8 +51,7 @@ export default function Book_list() {
 
   return (
     <>
-
-      <div class="product-container">
+       <div class="product-container">
         {getdata.slice(0, 4).map((data, index) => (
           <div class="product-card" key={index}>
             <a href={data.file?.file} target="_blank">
